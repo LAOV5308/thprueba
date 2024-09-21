d505d71749018ee68484f91c371543b0bb0a21d3
LAOV5308 <lo3977884@gmail.com>
2024-09-21 00:26:33 -0600

    Prueba vercel2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MyComponentComponent } from './my-component/my-component.component';
+import { MyComponent2Component } from './my-component2/my-component2.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MyComponent2Component, MyComponentComponent],
+  providers:[ ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/my-component2/my-component2.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  standalone:true,
+  selector: 'app-my-component2',
+  templateUrl: './my-component2.component.html',
+  styleUrls: ['./my-component2.component.css']
+})
+export class MyComponent2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
